6bd0b5f0b167031e4b6a7372c136e654eee18147
Carolina Teixeira <carolinatc1405@gmail.com>
2024-08-08 13:22:52 -0300

    Exceptions treated

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,26 @@
 import br.com.zipfinder.models.AddressWriter;
 import br.com.zipfinder.interfaces.Menu;
 import br.com.zipfinder.models.Search;
+import com.google.gson.JsonSyntaxException;
 
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        Menu menu = new Menu();
-        AddressWriter writer = new AddressWriter();
-        while (menu.isRunning()) {
-            menu.displayMenu();
-            menu.handleInput();
-        }
+    public static void main(String[] args) {
+       try {
+           Menu menu = new Menu();
+           AddressWriter writer = new AddressWriter();
+           while (menu.isRunning()) {
+               menu.displayMenu();
+               menu.handleInput();
+           }
 
-        if (!menu.getUrl().isBlank()) {
-            Search search = new Search(menu.getUrl());
-            writer.writeAddressToFile(search.getSearchedAddress());
-        }
+           if (!menu.getUrl().isBlank()) {
+               Search search = new Search(menu.getUrl());
+               writer.writeAddressToFile(search.getSearchedAddress());
+           }
+       } catch (IOException | InterruptedException | JsonSyntaxException e) {
+           throw new RuntimeException("Unable to obtain address from location or zip code");
+       }
     }
 }
